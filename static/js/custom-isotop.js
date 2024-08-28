b0cefb5d43dcabc377169739df2e958d6ff905f8
informatykdmd <informatyk@dmdbudownictwo.pl>
2024-08-28 10:13:14 +0200

    Working now

--- static/js/custom-isotop.js
+++ static/js/custom-isotop.js
@@ -5,30 +5,29 @@
     Copyright 2019
 *************************************************************/
 
-// $(window).on('load', function () {
+$(window).on('load', function () {
 
-// 	/*------------------
-// 	Isotope Filter
-// 	--------------------*/
-// 	var $container = $('.isotope_items');
-// 	$container.isotope();
+	/*------------------
+	Isotope Filter
+	--------------------*/
+	var $container = $('.isotope_items');
+	$container.isotope();
 
-// 	$('.portfolio-filter li').on("click", function () {
-// 		$(".portfolio-filter li").removeClass("active");
-// 		$(this).addClass("active");
-// 		var selector = $(this).attr('data-filter');
-// 		$(".isotope_items").isotope({
-// 			filter: selector,
-// 			animationOptions: {
-// 				duration: 750,
-// 				easing: 'linear',
-// 				queue: false,
-// 			}
-// 		});
-// 		return false;
-// 	});
-
-// });
+	$('.portfolio-filter li').on("click", function () {
+		$(".portfolio-filter li").removeClass("active");
+		$(this).addClass("active");
+		var selector = $(this).attr('data-filter');
+		$(".isotope_items").isotope({
+			filter: selector,
+			animationOptions: {
+				duration: 750,
+				easing: 'linear',
+				queue: false,
+			}
+		});
+		return false;
+	});
+});
 
 // $(document).ready(function(){
 //     var selectedFilter = "{{ selected_filter }}";
