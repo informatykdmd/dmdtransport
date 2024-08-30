b5f3a896ae398ab88aefec47257d16c10e3f4226
informatykdmd <informatyk@dmdbudownictwo.pl>
2024-08-30 09:30:00 +0200

    Working now

--- static/js/custom-isotop.js
+++ static/js/custom-isotop.js
@@ -29,32 +29,6 @@ $(window).on('load', function () {
 	});
 });
 
-// $(document).ready(function(){
-//     var selectedFilter = "{{ selected_filter }}";
-
-//     // Ustawienie początkowego filtra bez inicjalizacji Isotope
-//     if (selectedFilter) {
-//         $('.portfolio-filter li').removeClass('active');
-//         $('.portfolio-filter li[data-filter="' + selectedFilter + '"]').addClass('active');
-
-//         // Przefiltruj elementy już po załadowaniu strony
-//         $(window).on('load', function () {
-//             var $grid = $('.isotope_items').isotope({
-//                 itemSelector: '.col-lg-4',
-//                 layoutMode: 'fitRows',
-//                 filter: selectedFilter // Ustawienie filtra po załadowaniu Isotope
-//             });
-//         });
-//     } else {
-//         // Inicjalizacja Isotope bez wstępnego filtrowania, gdy selectedFilter nie jest ustawiony
-//         $(window).on('load', function () {
-//             $('.isotope_items').isotope({
-//                 itemSelector: '.col-lg-4',
-//                 layoutMode: 'fitRows'
-//             });
-//         });
-//     }
-// });
 
 
 
